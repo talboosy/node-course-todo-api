07dee102f6f07f6f31782d43e73f189122732b8c
DESKTOP-O1J837U\talbo <talboosy@gmail.com>
2019-01-09 17:38:18 +0200

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,29 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
+var password = '123abc';
 
-var token = jwt.sign(data,'123abc');
-console.log(token);
-var decoded = jwt.verify(token,'123abc');
-console.log('decoded',decoded)
+bcrypt.genSalt(10,(err,salt)=>{
+    bcrypt.hash(password, salt, (err,hash)=>{
+        console.log(hash);
+    });
+});
+
+var hashedPassword= '$2a$10$uFlPDgmuhhwzp9pJrOLf7.okD8hKefWiELnbPvAGLKbykUCWqOOPC';
+
+bcrypt.compare(password, hashedPassword, (err,res)=>{
+    console.log(res);
+});
+
+// var data = {
+//     id: 10
+// };
+
+// var token = jwt.sign(data,'123abc');
+// console.log(token);
+// var decoded = jwt.verify(token,'123abc');
+// console.log('decoded',decoded)
 // var message = 'User number 2';
 // var hash = SHA256(message).toString();
 
